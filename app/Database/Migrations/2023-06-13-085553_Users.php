81ce3f8f4b4e76290023ff11155cfd579c630758
wikinur <wikinurrochman30@gmail.com>
2023-06-13 19:59:44 +0700

    create project login with codeigniter 4

---
+++ app/Database/Migrations/2023-06-13-085553_Users.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Users extends Migration
+{
+    public function up()
+    {
+        // Kolom tabel Users
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'username' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'level' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+                'default' => 2,
+            ],
+            'image' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+            ],
+        ]);
+
+        // Primary Key
+        $this->forge->addKey('id', true);
+
+        // Buat Table
+        $this->forge->createTable('users', true);
+    }
+
+    public function down()
+    {
+        // Hapus Table
+        $this->forge->dropTable('users');
+    }
+}
